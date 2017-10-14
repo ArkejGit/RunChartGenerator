b8aa6b1a971c30ccfbe632aa2c9dd110b6df0c24
ArkejGit <robert.krawczyk92@gmail.com>
2017-10-14 16:53:21 +0200

    Enable displaying chart with results

--- run-chart-generator/src/app/modalContent/modal-content.component.ts
+++ run-chart-generator/src/app/modalContent/modal-content.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Injectable, OnInit } from '@angular/core';
 import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { GetDataService } from '../services/get-data.service';
+//import { ChartsModule } from 'ng2-charts/ng2-charts';
 
 @Injectable()
 @Component({
@@ -12,8 +13,15 @@ export class ModalContent implements OnInit {
 
   constructor(private getDataService: GetDataService, public activeModal: NgbActiveModal) {}
 
-  results:any;
+  results;
   loadAnimationFlag:boolean;
+  lineChartData:Array<any> = [
+    {data: []}
+  ];
+  lineChartOptions:any = {
+    responsive: true
+  };
+  lineChartType:string = 'line';
 
   ngOnInit() { 
 	this.loadResults();
@@ -23,9 +31,11 @@ export class ModalContent implements OnInit {
   this.loadAnimationFlag = true;
 	this.getDataService.getResults(this.link)
 			.subscribe(data => {
-				this.results = 'done';
+				this.lineChartData[0].data = data;
         this.loadAnimationFlag = false;
-        console.log(data);  				
+        console.log(data);	
 			});	
   }
+
+
 }
